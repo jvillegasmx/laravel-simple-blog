ac8875869732aefdbaf9cf5a886d4539257cb1c2
Javier Eduardo Villegas Aviles <javier.villegas@comunidad.unam.mx>
2024-08-13 23:21:02 -0600

    Moved to react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>iEdux ::  Web & Software Development</title>
 
         @viteReactRefresh
         @vite(['resources/js/app.jsx'])
